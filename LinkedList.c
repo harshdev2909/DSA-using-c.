4d96e3913f86f34628369dd6e8aa82b28ebc673a
harshdev2909 <harsh.dev2909@gmail.com>
2024-11-29 20:35:04 +0530

    linkedlist

--- LinkedList.c
+++ LinkedList.c
@@ -1,24 +1,34 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
 
 struct Node {
     int data;
     struct Node *Next;
 };
 struct  Node *start = NULL;
-struct Node *insert_at_beg(struct Node *)
+struct Node *insert_at_beg(struct Node *);
+struct Node *insert_at_end(struct Node *);
+struct Node *insert_at_loc(struct Node *);
 int main () {
     int choice;
     char ch;
     do
     {
         printf("\n 1. Enter the value of first Node");
+        printf("\n 2. Insert At Ending");
+        printf("\n 2. Insert At loc");
         scanf("%d",&choice);
         switch (choice)
         {
-        case 1 
+        case 1 :
             start = insert_at_beg(start);
             break;
-        
+        case 2 : 
+            start=insert_at_end(start);
+            break;
+        case 3 : 
+            start=insert_at_loc(start);       
         default:
             break;
         }
@@ -26,9 +36,6 @@ int main () {
     scanf("%d",&ch);
     ch=getch();    
     } while (ch='y');
-    
-    
-    return 0;
 }
 
 struct Node *insert_at_beg(struct Node *start)
@@ -36,18 +43,71 @@ struct Node *insert_at_beg(struct Node *start)
 	struct Node *temp;
 	temp=(struct Node*)malloc(sizeof(struct Node));
 	printf("\nEnter the data in Node");
-	scanf("%d",&temp->info);
+	scanf("%d",&temp->data);
 	if(start==NULL)
 	{
 		start=temp;
-		start->next=NULL;
+		start->Next=NULL;
 		printf("\nIt is our First Node");
 	}
 	else
 	{
-		temp->next=start;
+		temp->Next=start;
 		start=temp;
 	}
 	// printList(start);
 	return start;
+}
+struct Node *insert_at_end(struct Node *start)
+{
+    struct Node *temp;
+	temp=(struct Node*)malloc(sizeof(struct Node));
+	printf("\nEnter the data in Node");
+	scanf("%d",&temp->data);
+	if(start==NULL)
+	{
+		start=temp;
+		start->Next=NULL;
+		printf("\nIt is our First Node");
+	}
+    else
+    {
+        struct Node *ptr;
+        while(ptr->Next!=NULL)
+        {
+            ptr=ptr->Next;
+        }
+        ptr->Next=temp;
+        temp->Next=NULL;
+    }
+    return start;
+}
+
+struct Node *insert_at_loc(struct Node *start)
+{
+    struct Node *temp;
+	temp=(struct Node*)malloc(sizeof(struct Node));
+	printf("\nEnter the data in Node");
+	scanf("%d",&temp->data);
+	if(start==NULL)
+	{
+		start=temp;
+		start->Next=NULL;
+		printf("\nIt is our First Node");
+	}
+    else 
+    {
+        int index;
+        printf("\nEnter the Index");
+	    scanf("%d",&index);
+        struct Node *ptr;
+        int i = 0;
+        while(i!=index-1)
+        {
+            ptr=ptr->Next;
+        }
+        ptr->Next=temp->Next;
+        ptr->Next=temp;
+    }
+    return start;
 }
